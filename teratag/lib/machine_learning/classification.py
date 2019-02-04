update is-TPG,min-max-normalization,graph,colorcode and fix pca

teratag/lib/machine_learning/classification.py
@@ -78,10 +78,10 @@ def pCA(x_all, y_all):
     # 主成分をプロットする
     for label in np.unique(targets):
         plt.scatter(transformed[targets == label, 0],
-                    transformed[targets == label, 1], label=label)
+                    transformed[targets == label, 1], label='{}mm'.format(label*0.5))
     plt.legend(loc='upper right',
-               bbox_to_anchor=(1.05, 0.5, 0.5, .100),
-               borderaxespad=0., )
+               bbox_to_anchor=(1,1),
+               borderaxespad=0.5,fontsize = 10)
     plt.title('principal component')
     plt.xlabel('pc1')
     plt.ylabel('pc2')

teratag/main_transmittance_reflectance_is_TPG.py
@@ -1,36 +1,39 @@
 import numpy as np
-from TDS.lib.TDS_read import allread
-from TDS.lib.train_test_split import train_test_split
-from TDS.lib.machine_learning.classification import svm,kNN,pCA
-from TDS.lib.visualization import colorcode
+from lib.Allread import allread
+from lib.train_test_split import train_test_split
+from lib.machine_learning.classification import svm,kNN,pCA
+from lib.visualization import colorcode
 
 y_all = []
 flag = 0
 #mainデータを読み込む。
-for i in range(1,4):
-    for j in range(1,6):
+for i in range(1,5):
+    i = i*0.5
+    for j in range(1,5):
         try:
-            x = allread('/Users/ryoya/kawaseken/20190123/2019_0123_{0}mm_{1}.txt'.format(i,j)).Frequency_transmittance('/Users/ryoya/kawaseken/20190123/2019_0123_ref_1.txt')
+            x = allread('reflectance').Frequency_trans_reflect_is_TPG('/Users/ryoya/kawaseken/20190201_fix/PE_{0}mm_{1}.txt'.format(i,j),
+                '/Users/ryoya/kawaseken/20190201/ref.txt',1.4,1.5)
             if flag == 0:
                 x_all = x
                 flag += 1
             else:
                 x_all = np.append(x_all, x, axis=0)
-            y_all.append(i)
+            #y_allの値がint出ないとsvm,pcaの可視化が上手くいかないので0.5mmの場合は*2などをして元に戻す。
+            y_all.append(i*2)
         except FileNotFoundError as e:
             print(e)
 #train_test_split(特徴量,目的関数,1つの厚さにおけるtrainデータの数)
 train_x,train_y,test_x,test_y = train_test_split(x_all,y_all,1)
 
-#print(train_x)
-#print(train_y)
+print(train_x)
+print(train_y)
 #print(test_x)
 #print(test_y)
-print(x_all)
+#print(x_all)
 #print(y_all)
 #referenceのカラーコード
 #カラーコードのタグの数width=4,length=4の場合16個のタグに対応
-width = 4
+width = 3
 length = 4
 colorcode(test_y,width,length)
 #SVM
@@ -45,4 +48,4 @@ transformed, targets = pCA(x_all, y_all)
 train_x_pca,train_y_pca,test_x_pca,test_y_pca = train_test_split(transformed,targets,1)
 
 best_pred = svm(train_x_pca, train_y_pca, test_x_pca, test_y_pca)
-colorcode(best_pred, 4, 4)
+colorcode(best_pred, width, length)