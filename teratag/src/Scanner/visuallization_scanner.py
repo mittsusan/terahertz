b014539459e2ae4b8a75c52456bdffe9e4e5221e
mittsusan <ryoyaanpan@gmail.com>
2019-04-15 16:10:31 +0900

    update graph_Time_intensity_everymm and min_max_normalization_TDS and fix main_Time_intensity and main_transmittance_reflectance

---
+++ teratag/src/Scanner/visuallization_scanner.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+#os.chdir('')
+df = pd.read_csv('../../../tmp.csv', header=None, skiprows=2)
+a_df = df.values
+print(a_df)
+
+#画像の表示
+plt.imshow(a_df, cmap = 'gray', vmin = 0, vmax = 255, interpolation = 'none')
+# => plt.imshow(img_rgb, interpolation = 'none') と同じ
+
+plt.show()
