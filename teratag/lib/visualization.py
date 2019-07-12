41720a04a5e74fc2a26885f89d060ac8fa86485a
mittsusan <ryoyaanpan@gmail.com>
2019-07-12 16:39:04 +0900

    update pca and take argument filename

--- teratag/lib/visualization.py
+++ teratag/lib/visualization.py
@@ -21,22 +21,6 @@ def colorcode(best_pred, x, y):
         position_y.append(first_position_y)
         first_position_y += 110
     for i, j in enumerate(best_pred):
-        '''
-        if j != j_save:
-            color_R = color_G*2
-            color_G = color_B*2
-            color_B = color_R*2
-
-
-        if color_R >= 255:
-            color_R = 10
-
-        if color_G >= 255:
-            color_G = 10
-
-        if color_R >= 255:
-            color_G = 10
-        '''
 
         if j == 1:
             color_R = 255
@@ -71,39 +55,11 @@ def colorcode(best_pred, x, y):
 
 
         for k in range(y):
-            #print('loopstart')
-            #print(k)
-            #print('kkk{}'.format(k))
-            #print('x{}'.format(x))
-            #print('i:{}'.format(i))
             if int(i) < int(x) * int(k+1):
-                #print('描けた')
-                #print(position_x)
-                #print(position_y)
-                #print('k{}'.format(k))
-                #print(j)
                 draw.rectangle((position_x[k], position_y[k], position_x[k] + 100, position_y[k+1] - 10),
                                fill=(color_R, color_G, color_B))
                 position_x[k] += 110
                 break
-            #print('描けなかった')
-
-
-        '''
-        if i < x:
-            draw.rectangle((position_x[0], position_y[0], position_x[0] + 100, position_y[1]-10), fill=(color_R, color_G, color_B))
-            position_x[0] += 110
-        elif i < x * 2:
-            draw.rectangle((position_x[1], position_y[1], position_x[1] + 100, position_y[2]-10), fill=(color_R, color_G, color_B))
-            position_x[1] += 110
-        elif i < x * 3:
-            draw.rectangle((position_x[2], position_y[2], position_x[2] + 100, position_y[3]-10), fill=(color_R, color_G, color_B))
-            position_x[2] += 110
-        elif i < x * 4:
-            draw.rectangle((position_x[3], position_y[3], position_x[3] + 100, position_y[4]-10), fill=(color_R, color_G, color_B))
-            position_x[3] += 110
-        
-        '''
 
         #j_save = j
     plt.imshow(np.array(im))
--- teratag/src/is_TPG/main_transmittance_reflectance_is_TPG.py
+++ teratag/src/is_TPG/main_transmittance_reflectance_is_TPG.py
@@ -7,18 +7,24 @@ from lib.train_test_split import train_test_split,decide_test_number
 from lib.machine_learning.classification import svm,kNN,pCA
 from lib.visualization import colorcode
 
+file_name_list = [] #filenameの保持
 date_dir = '/Users/ryoya/kawaseken'
-shielding_material = '/cardboard'
+shielding_material = '/EMS_PE_zink'
 sensitivity = 'nosensitivity'
-from_frequency = 1.0
-to_frequency = 2.1
+from_frequency = 1.4
+to_frequency = 1.5
 thickness = 'mm'
-y_axis = 'intensity[a.u.]'
+y_axis = 'transmittance'
 y_all = []
 flag = 0
 #mainデータを読み込む。
-last_type = 6 #使用する種類
-last_num = 4 #最後の種類の使用するファイル数
+last_type = 4 #使用する種類
+last_num = 11 #最後の種類の使用するファイル数
+#カラーコードのタグの数width=4,length=4の場合16個のタグに対応
+width = 5
+length = 7
+test_number = 5
+pca_third_argument = 0 #PCAの第3引数で0の場合厚み、それ以外は糖類に設定されています。
 
 #データの読み込み
 for i in range(1,last_type+1):
@@ -28,6 +34,8 @@ for i in range(1,last_type+1):
         try:
             x = allread(y_axis,str(i)+thickness,i,j,last_type,last_num).Frequency_trans_reflect_is_TPG(date_dir + shielding_material + '/' + sensitivity + '/' + str(i) + '/' + str(j) + '.txt',
                     date_dir + shielding_material + '/ref.txt',from_frequency,to_frequency)
+
+            file_name_list.append(j)
 
             if flag == 0:
                 x_all = x
@@ -43,8 +51,8 @@ for i in range(1,last_type+1):
             print(e)
 
 #train_test_split(特徴量,目的関数,1つの厚さにおけるtrainデータの数)
-train_x,train_y,test_x,test_y = train_test_split(x_all,y_all,1)
-#train_x,train_y,test_x,test_y = decide_test_number(x_all,y_all,3)
+#train_x,train_y,test_x,test_y = train_test_split(x_all,y_all,1)
+train_x,train_y,test_x,test_y = decide_test_number(x_all,y_all,test_number)
 #train_x, test_x, train_y, test_y = train_test_split(x_all, y_all, test_size=3)
 
 print(type(train_x))
@@ -54,9 +62,7 @@ print(type(train_y))
 #print(x_all)
 #print(y_all)
 #referenceのカラーコード
-#カラーコードのタグの数width=4,length=4の場合16個のタグに対応
-width = 3
-length = 7
+
 colorcode(test_y,width,length)
 #SVM
 print('\nSVM')
@@ -68,10 +74,10 @@ best_pred=kNN(train_x,train_y,test_x,test_y)
 colorcode(best_pred,width,length)
 # PCA-SVM
 print('\nPCA-SVM')
-#厚みで識別する際はPCAの第3引数を0に
-transformed, targets = pCA(x_all, y_all,1)
 
-train_x_pca,train_y_pca,test_x_pca,test_y_pca = decide_test_number(transformed,targets,3)
+transformed, targets = pCA(x_all, y_all, pca_third_argument,file_name_list)
+
+train_x_pca,train_y_pca,test_x_pca,test_y_pca = decide_test_number(transformed,targets,test_number)
 
 best_pred = svm(train_x_pca, train_y_pca, test_x_pca, test_y_pca)
 colorcode(best_pred, width, length)
